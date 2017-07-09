10f5d1262784c66fe59a09fa11d1a8bd05548ac2
Elliot Plant <elliotaplant@gmail.com>
2017-07-08 22:44:06 -0700

    More scrubbing

--- models/User.js
+++ models/User.js
@@ -12,19 +12,10 @@ const userSchema = new mongoose.Schema({
   passwordResetExpires: Date,
 
   facebook: String,
-  twitter: String,
   google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
-  steam: String,
-  tokens: Array,
 
   profile: {
     name: String,
-    gender: String,
-    location: String,
-    website: String,
     picture: String
   }
 }, {
